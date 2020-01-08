fa11417745a534bdb80589e2acfe83f0d512cda5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-08 11:51:02 +0000

    Deploy abhishek-sisodiya/Git-Page to github.com/abhishek-sisodiya/Git-Page.git:gh-pages

---
+++ precache-manifest.f3a4ad8da59529092a850bf26d938502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d4ebfd1f9d45ba7056f",
+    "url": "/GitPage-Travis/static/css/main.8c819d89.chunk.css"
+  },
+  {
+    "revision": "5d4ebfd1f9d45ba7056f",
+    "url": "/GitPage-Travis/static/js/main.5d4ebfd1.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/GitPage-Travis/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "3d592a92e84e767f1943",
+    "url": "/GitPage-Travis/static/js/runtime~main.3d592a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GitPage-Travis/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "25c4176117a7212d81bd24a74ca900f9",
+    "url": "/GitPage-Travis/index.html"
+  }
+];
